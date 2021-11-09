28afbea48678deb27e5781764ea46c9b61099f3f
Daniel Ocampo <ochampo77@gmail.com>
2021-11-08 21:06:18 -0800

    Add project files.

---
+++ SringAlgorithms/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SringAlgorithms;
+
+ValidateStrings validate = new ValidateStrings();
+
+Console.WriteLine(validate.IsUppercase("hello"));
+Console.WriteLine(validate.IsUppercase("HELLO"));
+Console.WriteLine(validate.IsLowercase("hello"));
+Console.WriteLine(validate.IsLowercase("HELLO"));
---
+++ SringAlgorithms/ValidateStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SringAlgorithms
+{
+    public class ValidateStrings
+    {
+
+     public Boolean IsUppercase(string s)
+        {
+         return s.All(char.IsUpper);
+        }
+
+      public  Boolean IsLowercase(string s)
+        {
+            
+                return s.All(char.IsLower);
+        }
+                
+
+
+
+
+    }
+}

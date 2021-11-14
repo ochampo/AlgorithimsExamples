4f149ec0a69a70496004389a5a7e1483189bc314
Daniel Ocampo <ochampo77@gmail.com>
2021-11-13 19:40:52 -0800

    wrong name, not a tree. found bug, incorrect comparision on if statement.

---
+++ ArrayAlgorithms/BinerySearchExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public class BinerySearchExample
+    {
+
+        public Boolean BinarySearch(int[] inputArray, int item)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+
+            while(min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (item == inputArray[mid])
+                    return true;
+                else if (item < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                } 
+
+
+            }
+
+            return false;
+        }
+
+
+
+
+    }
+}
--- ArrayAlgorithms/Program.cs
+++ ArrayAlgorithms/Program.cs
@@ -4,12 +4,17 @@ using ArrayAlgorithms;
 
 int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 //LinerSearch linerSearch = new LinerSearch(ref arr, ref 4);
-LinerSearch linerSearch = new LinerSearch();
+//LinerSearch linerSearch = new LinerSearch();
 
-int? x = linerSearch.LinerSearchFindingData(arr,5);
-Console.WriteLine(x);
-int Item = Array.Find(arr,element => element == 3);
-Console.WriteLine("Array Example"+ Item);
-int[] items2 =  Array.FindAll(arr, element => element >=5);
-Array.ForEach(items2,Console.WriteLine);
+//int? x = linerSearch.LinerSearchFindingData(arr,5);
+//Console.WriteLine(x);
+//int Item = Array.Find(arr,element => element == 3);
+//Console.WriteLine("Array Example"+ Item);
+//int[] items2 =  Array.FindAll(arr, element => element >=5);
+//Array.ForEach(items2,Console.WriteLine);
+
+BinerySearchExample binerySearch  = new BinerySearchExample();
+Console.WriteLine(binerySearch.BinarySearch(arr,4));
+Console.WriteLine(Array.BinarySearch(arr,5));
+
 

e4cfd73b998358f4c1db5fa911c5775b370bf9c9
Daniel Ocampo <ochampo77@gmail.com>
2021-11-13 18:47:25 -0800

    Liner search algorithm for reteriving items

---
+++ ArrayAlgorithms/LinerSearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public class LinerSearch
+    {
+        //with constructor WIP
+        //public  LinerSearch(ref int[] input, ref int n)
+        // {
+
+
+        // }
+
+
+        // public int? FindValue()
+        // {
+        //     int[] input =null;
+        //     int n = 0;
+        //     LinerSearch linerSearch = new LinerSearch(ref input, ref n);
+
+        //     foreach (int current in input)
+        //     {
+
+        //         if (n == current)
+        //         {
+        //             return n;
+        //         }
+        //     }
+
+        //     return null;
+
+
+        // }
+
+
+        public int? LinerSearchFindingData(int[] input, int n)
+        {
+            foreach (int current in input)
+            {
+                if (n == current)
+                {
+                    return n;
+                }
+
+            }
+            return null;
+
+        }
+
+
+
+
+    }
+}
---
+++ ArrayAlgorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayAlgorithms;
+
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+//LinerSearch linerSearch = new LinerSearch(ref arr, ref 4);
+LinerSearch linerSearch = new LinerSearch();
+
+int? x = linerSearch.LinerSearchFindingData(arr,5);
+Console.WriteLine(x);
+int Item = Array.Find(arr,element => element == 3);
+Console.WriteLine("Array Example"+ Item);
+int[] items2 =  Array.FindAll(arr, element => element >=5);
+Array.ForEach(items2,Console.WriteLine);
+

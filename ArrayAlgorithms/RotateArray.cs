83f216911b5af3c23c613df9de48b590a3517fd5
Daniel Ocampo <ochampo77@gmail.com>
2021-11-17 23:52:53 -0800

    working on array swaps

---
+++ ArrayAlgorithms/RotateArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public class RotateArray
+    {
+        static void RotateArrayLeft (int [] input)
+        {
+            int[] reversed = new int[input.Length];
+            int temp = input[0];
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed[i] = input[input.Length - i - 1];
+
+            }
+            
+
+        }
+
+
+
+    }
+}

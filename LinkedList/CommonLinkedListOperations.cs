fc3ead05a18335b02df80d905c12a45b69691ac3
Daniel Ocampo <ochampo77@gmail.com>
2021-11-18 22:54:28 -0800

    linkList examples

---
+++ LinkedList/CommonLinkedListOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CommonLinkedListOperations
+    {
+        public void StringLinkList()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+
+            list.AddLast("howdy");
+
+
+        }
+
+
+    }
+}

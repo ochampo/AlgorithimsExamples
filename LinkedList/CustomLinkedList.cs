b04c6e5490290ccf2d1bfa48194c5076c5c83906
Daniel Ocampo <ochampo77@gmail.com>
2021-11-24 23:31:10 -0800

    linkedList Custome algorithim.

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+      public  Node? head;
+
+        public class Node
+        {
+
+            public int data;
+
+            public Node next; 
+
+
+            public Node(int d ) 
+            { 
+                data = d; 
+            }
+        }
+        public void deleteBackHalf()
+        {
+            if(head==null || head.next == null )
+            {
+                head = null;
+            }
+            Node slow = head;
+            Node fast = head;
+            Node prev = null;
+            
+            while(fast != null && slow.next != null )
+            {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            
+            }
+            prev.next = null;
+        }
+
+        public void displayContents()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + "->");
+                current = current.next;
+            }
+
+        }
+
+
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+using static LinkedList.CustomLinkedList;
 Console.WriteLine("Hello, World!");
+
+ 
+CustomLinkedList list = new CustomLinkedList();
+
+Node firstnode = new Node(3);
+Node secondnode = new Node(4);
+Node thirdnode = new Node(5);
+Node fourthnode = new Node(6);
+
+list.head = firstnode;
+firstnode.next = secondnode;
+secondnode.next = thirdnode;
+thirdnode.next = fourthnode;
+list.displayContents();
+list.deleteBackHalf();
+Console.WriteLine();
+list.displayContents();

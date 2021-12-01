d458537ed0a243ebda7a2de916f94236106c1dc0
Daniel Ocampo <ochampo77@gmail.com>
2021-12-01 00:05:22 -0800

    generate binary numbers with a queue.

---
+++ QueuesAndStacks/PrintBinary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuesAndStacks
+{
+    public class PrintBinary
+    {
+        public void printBinary(int n )
+        {
+            if ( n <= 0 )
+            { 
+                return;
+            }
+
+
+            Queue<int> queue = new Queue<int>(); 
+            queue.Enqueue(1);
+            for(int i = 0; i <n ; i++)
+            {
+                int current = queue.Dequeue();
+                Console.WriteLine(current);
+                queue.Enqueue(current * 10);
+                queue.Enqueue(current * 10 + 1);
+            }
+
+            Console.WriteLine();
+
+
+        }
+
+
+
+    }
+}
--- QueuesAndStacks/Program.cs
+++ QueuesAndStacks/Program.cs
@@ -4,6 +4,13 @@ using QueuesAndStacks;
 Console.WriteLine("Hello, World!");
 
 QueueTest queueTest = new QueueTest();
+PrintBinary printBinary = new PrintBinary();
+printBinary.printBinary(4);
+printBinary.printBinary(10);
+printBinary.printBinary(5);
+printBinary.printBinary(3);
 
 
 queueTest.QueueImplementation();
+
+

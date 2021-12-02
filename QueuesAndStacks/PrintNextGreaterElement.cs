54dbacefb98431486f41b9b960026ffe0dcc75e1
Daniel Ocampo <ochampo77@gmail.com>
2021-12-01 23:31:58 -0800

    Creating a stack example not complete, but the point is to find the greatest value in an array.

---
+++ QueuesAndStacks/PrintNextGreaterElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuesAndStacks
+{
+    public class PrintNextGreaterElement
+    {
+        public void NumbersChecker(int [] arr)
+        {
+
+            if(arr.Length == 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+        Stack<int> stack = new Stack<int>();
+        stack.Push(arr[0]);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int next = arr[i];
+                stack.Push(arr[i]);
+                if (stack.Count > 0)
+                {
+                    int popped = stack.Pop();
+
+                    while (popped < next)
+                    {
+                        Console.WriteLine(popped + "---->" + next );
+                        popped = stack.Pop();
+                     }
+                }
+
+                stack.Push(next);
+            }
+        
+        
+        
+        
+        
+        }
+
+
+
+
+
+    }
+}

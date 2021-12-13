b93d48ddd528efd4896b0f0bdd870d11d238d6ce
Daniel Ocampo <ochampo77@gmail.com>
2021-12-12 22:37:15 -0800

    added more functionality to the frequency function. not tested.;

--- hashBasedStructures/DisplayFrequency.cs
+++ hashBasedStructures/DisplayFrequency.cs
@@ -6,21 +6,39 @@ using System.Threading.Tasks;
 
 namespace hashBasedStructures
 {
-    public  class DisplayFrequency
+    public class DisplayFrequency
     {
-        static void Frequency(int[] arr)
+        static void frequency(int[] arr)
         {
-        for(int i =0; i < arr.Length; i++)
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+
+
+            for (int i = 0; i < arr.Length; i++)
             {
-                
+                if (freq.ContainsKey(arr[i]))
+                {
+                    freq[arr[i]]++;
+                }
+                else
+                {
+                    freq[arr[i]] = 1;
+                }
+
+
             }
-        
-        
-        } 
 
-
-
+            foreach (KeyValuePair<int,int> x in freq)
+            {
+                Console.WriteLine(x.Key + " " + x.Value);
+            }
+        }
 
 
     }
+
+
+
+
+
+}
 }
